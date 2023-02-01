1f63c6a5e9c4832f3b453f5ecaa058d6e41c3830
cyberprophet <cyberprophet@hanmail.net>
2023-02-02 00:30:19 +0900

    cyberprophet

---
+++ src/app.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('gpascore.csv')
+
+data = data.dropna()
+
+y = data['admit'].values
+
+x = []
+
+
+for i, rows in data.iterrows():
+
+    x.append([rows['gre'], rows['gpa'], rows['rank']])
+
+
+model = tf.keras.models.Sequential(
+    [tf.keras.layers.Dense(64, activation='tanh'),
+     tf.keras.layers.Dense(128, activation='tanh'),
+     tf.keras.layers.Dense(1, activation='sigmoid')])
+
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(np.array(x), np.array(y), epochs=1000)
+
+predict = model.predict([[750, 3.70, 3], [400, 2.2, 1]])
+
+print(predict)
